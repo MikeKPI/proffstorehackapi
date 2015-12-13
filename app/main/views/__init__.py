c3ecbd4aee71fcb37fe974f607be51f3ffd57b76
cmgc <unlesnick@gmail.com>
2015-12-13 09:57:06 +0200

    added new resource to api

--- app/main/views/__init__.py
+++ app/main/views/__init__.py
@@ -2,6 +2,7 @@ from app import api
 from .auth import Auth
 from project import Project, ProjectList
 from task import Task, TaskList
+from comment import Comment, CommentList
 
 api.add_resource(Auth, "/login", endpoint="login")
 api.add_resource(ProjectList, "/projects", endpoint="project")
@@ -9,3 +10,6 @@ api.add_resource(Project, "/projects/<string:id>", endpoint="project")
 
 api.add_resource(TaskList, "/tasks", endpoint="task")
 api.add_resource(Task, "/tasks/<string:id>", endpoint="task")
+
+api.add_resource(CommentList, "/comments", endpoint="comment")
+api.add_resource(Comment, "/comments/<string:id>", endpoint="comment")
