28d3be4595e3668ec4237306c3891ae78112c593
mrkiril <mrkiril@ukr.net>
2015-12-13 15:23:39 +0200

    index
    INDEX od Users

--- api/trello.py
+++ api/trello.py
@@ -30,7 +30,7 @@ def order_task( access_token ):
     #Access_Token = {"x-access-token": access_token }
     headers = {'Content-Type':'application/json', 'x-client-code':client_code ,"x-access-token": access_token }
     r = requests.get(url, headers=headers )
-    print( r.json())
+    #print( r.json())
     print ()
     return list(map(need_field, r.json()["projects"].values()))
     
@@ -61,11 +61,61 @@ def st_color(start , end):
     else:
        return "#ffffff"
 
+def all_index_of_freelanser( name ):
+    url1 = 'https://proffstore.com/api/v1/search/'
+    url2 = "?type=user"
+    client_code = "a34f65b2c464"
+    #Access_Token = {"x-access-token": access_token }
+    headers = {'Content-Type':'application/json', 'x-client-code':client_code }
+    r = requests.get(url1+name+url2, headers=headers )
+    json1 = r.json()
+    json2 = list(map(index_of_freelanser , json1["result"]["users"].values()))
+    #print (json1)
+    if len(json2) != 0:
+       return json2
+    else :
+       print("list is empty" ) 
+
+
+def  index_of_freelanser( person ):
+    #print(person)
+    print ({"id" : person["id"], "firstName": person["firstName"] , "lastName": person["lastName"] , "index": index(person["id"]) } )
+    return ({"id" : person["id"], "firstName": person["firstName"] , "lastName": person["lastName"], "index": index(person["id"]) } )
+
+def index(id):
+    url1 = 'https://proffstore.com/api/v1/review/'
+    portion="?portion=1000"
+    client_code = "a34f65b2c464"
+    #Access_Token = {"x-access-token": access_token }
+    headers = {'Content-Type':'application/json', 'x-client-code':client_code }
+    r = requests.get(url1+str(id)+portion, headers=headers )
+    json1 = r.json()
+    a = list(map(awerage_index , json1["reviews"].values() ))
+    
+    return sum(a)/len(a)
+
+def awerage_index(indexs):
+    p1 = indexs.get("parametr1")/10
+    
+
+    p2 = indexs.get("parametr2")/10
+    p3 = indexs.get("parametr3")/10
+    p4 = indexs.get("parametr4")/10
+    p5 = indexs.get("parametr5")/10
+    print(p1, p2, p3 , p4, p5)
+    return p2*p1*(p5*p3*p2+p5*p3+p2)
+
+
+
+
+
+
 print ("1.Access_token")
 ACCESS_TOKEN = Access_token("mrkiril@ukr.net","123ss456")
 print ("2. to_do_task")
 
-print ( order_task(ACCESS_TOKEN) )
+#print ( order_task(ACCESS_TOKEN) )
 
+all_index_of_freelanser("kovrova")
 #order_task(ACCESS_TOKEN)
 
